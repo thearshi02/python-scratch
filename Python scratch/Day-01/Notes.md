9e130337fa85d7ad04340dbd9114a3d4184bb2ad
Arshi <arshiidrisi0202@gmail.com>
2026-06-22 11:59:23 +0530

    completed day 1 task

---
+++ Python scratch/Day-01/Notes.md
@@ -0,0 +1,14 @@
+#Variable 
+ it is the space where we store the values and the variable name start form the character .. it can store the values of different data types like integer, float, boolean etc. 
+
+ #datatypes
+
+int= it store the integer values
+float = it store the decimal values
+bool = it store the boolean value i.e True and False.
+
+#dynamic Typing 
+Detect the type of the datatype 
+
+#type()
+it is used to check the datatype of the variable.
---
+++ Python scratch/Day-01/variables_practice.py
@@ -0,0 +1,20 @@
+# creating the variables and storing values
+
+item_name = "Shirt"
+price =  1999.99
+quantity = 6
+is_in_stock = True
+
+#print the variables values and types of the variables
+
+print ("item= ", item_name)
+item_type = print(type(item_name))
+
+print ("price= ", price)
+item_type = print(type(price))
+
+print ("quantity= ", quantity)
+item_type = print(type(quantity))
+
+print ("is_in_stock ", is_in_stock)
+item_type = print(type(is_in_stock))
